926a91b38efb06e4eac0b4d366a0446e43b19afb
Trần Mạnh Huy <manhhuy0107@gmail.com>
2024-10-17 16:04:15 +0700

    hihi

---
+++ javane.js
@@ -0,0 +1,5 @@
+document.getElementById('playMusic').addEventListener('click', function() {
+    alert('Bị lừa ời! 🎉');
+    const song = document.getElementById('birthdaySong');
+    song.play();
+});
